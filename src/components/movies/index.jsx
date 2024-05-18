658cc8945526db1ee0bb3e76ea155f8c3bafd13f
Marcos <m.vinicius.sr11@gmail.com>
2024-05-17 23:06:11 -0300

    estilo finalizado

--- src/components/movies/index.jsx
+++ src/components/movies/index.jsx
@@ -1,24 +1,34 @@
 /* eslint-disable react/prop-types */
 
+import { useState } from 'react'
 import './style-movies.css'
 
 
 export default function Movies({movie, handdleRemoveMovie, handdleEditMovie}){
+  const [showDropdown, setShowDropdown] = useState(false);
+  const trueDropdown = () => setShowDropdown(true);
+  const falseDropdown = () => setShowDropdown(false);
 
+  return(
+    <div className='movie'>
 
-
-    return(
-        <div className='movie'>
-          <details className='movie-button'> {/*tres pontinhos*/}
-            <summary><i className="fa-solid fa-ellipsis-vertical"></i></summary>
-            
+     <div className='dropdown-area' onMouseLeave={falseDropdown}>
+      <button className='dropdown-button' onMouseEnter={trueDropdown} >
+          <i className="fa-solid fa-ellipsis-vertical"></i>
+        </button>
+          
+          {showDropdown && (
+            <div className='dropdown' onMouseLeave={falseDropdown}>
             <button onClick={()=> handdleRemoveMovie(movie.id)}>Remover</button>
             <button onClick={()=> handdleEditMovie(movie.id)}>Editar</button>
-          </details>
+          </div>
+          )}
+     </div>
 
-          <h3>{movie.title}</h3>
-          <p>{movie.synopsis}</p>
-          <iframe src={movie.trailler} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-        </div>
-    )
+
+      <h3>{movie.title}</h3>
+      <p>{movie.synopsis}</p>
+      <iframe src={movie.trailler} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+    </div>
+  )
 }
